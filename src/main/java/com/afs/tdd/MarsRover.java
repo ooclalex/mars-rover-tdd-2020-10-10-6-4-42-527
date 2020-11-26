17d2d92608213b06864fb20d06af13a4bba05860
Alex Liu <liual@oocl.com>
2020-11-26 16:57:50 +0800

    [MarsRover][Alex Liu] test: added case where command is [N 0 0][M]

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,28 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int xPosition;
+    private int yPosition;
+    private char direction;
+
+    public MarsRover(int xPosition, int yPosition, char direction) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.direction = direction;
+    }
+
+    public void executeCommands(String m) {
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+    public int getY() {
+        return 0;
+    }
+
+    public char getDirection() {
+        return 0;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,21 @@
 package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
-    @Test
-    void write_your_first_test() {
 
+    @Test
+    public void should_return_y_1_when_execute_command_given_N_0_0_M() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'N');
+
+        // when
+        marsRover.executeCommands("M");
+
+        // then
+        assertEquals(0, marsRover.getX());
+        assertEquals(1, marsRover.getY());
+        assertEquals('N', marsRover.getDirection());
     }
 }
