98b389782653178c889d7bf3efb6f621eb99c2f1
Alex Liu <liual@oocl.com>
2020-11-26 17:06:07 +0800

    [MarsRover][Alex Liu] test: added case where command is [S 0 0][M]

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -18,4 +18,18 @@ class DemoTest {
         assertEquals(1, marsRover.getY());
         assertEquals("N", marsRover.getDirection());
     }
+
+    @Test
+    public void should_return_y_neg_1_when_execute_command_given_S_0_0_M() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+
+        // when
+        marsRover.executeCommands("M");
+
+        // then
+        assertEquals(0, marsRover.getX());
+        assertEquals(-1, marsRover.getY());
+        assertEquals("S", marsRover.getDirection());
+    }
 }
